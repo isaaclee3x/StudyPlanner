b136daee32bfeb7404253912f62e607e1f1b47e5
isaaclee <75001370+isaaclee3x@users.noreply.github.com>
2023-03-09 23:23:17 +0800

    Initial Commit

---
+++ StudyPlanner/StudyPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyPlannerApp.swift
+//  StudyPlanner
+//
+//  Created by Isaac Lee Jing Zhi on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct StudyPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
