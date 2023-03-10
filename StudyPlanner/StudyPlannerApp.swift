a087f75b178991d1f45c446a8c1b14e7835ef02a
isaaclee <75001370+isaaclee3x@users.noreply.github.com>
2023-03-10 10:41:39 +0800

    Added persistence to the courses (store each course locally on device)

--- StudyPlanner/StudyPlannerApp.swift
+++ StudyPlanner/StudyPlannerApp.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 @main
 struct StudyPlannerApp: App {
+    
+    @StateObject private var courseStore = CourseStorage()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear {
+                    courseStore.load { result in
+                        switch result {
+                        case .failure(let error):
+                            fatalError(error.localizedDescription)
+                        case .success(let courses):
+                            courseStore.courses = courses
+                        }
+                    }
+                }
         }
     }
 }
