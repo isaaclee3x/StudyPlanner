210ac8cc6456cf234e8e107e8dacf030f8a081b4
isaaclee <75001370+isaaclee3x@users.noreply.github.com>
2023-03-10 10:32:56 +0800

    Creating objective and course structs

--- StudyPlanner/Course.swift
+++ StudyPlanner/Course.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+struct Course: Identifiable, Equatable, Codable {
+    
+    var id = UUID()
+    
+    var name: String
+    var colour = [0.0, 0.0, 0.0] {
+        didSet {
+            if colour.count > 3 {
+                print("Error: colour cannot have more than the 3 rgb parameters")
+                colour = oldValue
+            }
+        }
+    }
+    
+    var objectives: [Objective]
+    
+    var startOfCourse: Date 
+    var endOfCourse: Date {
+        var date = startOfCourse
+        if objectives == [] { print("Error: There are no objectives")}
+        else {
+            for i in objectives {
+                date = date.addingTimeInterval(i.duration.timeInterval)
+            }
+        }
+        
+        return date
+    }
+}
--- StudyPlanner/Objective.swift
+++ StudyPlanner/Objective.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import SwiftUI
+import SwiftDate
+
+class Objective: Identifiable, Equatable, Codable {
+    static func == (lhs: Objective, rhs: Objective) -> Bool {
+        return lhs.title == rhs.title && lhs.description == rhs.description && lhs.duration == rhs.duration
+    }
+    
+    var id: Int
+    
+    init(title: String, description: String) {
+        self.id = 0
+        self.title = title
+        self.description = description
+        self.duration = 1.hours
+    }
+    
+    var title: String
+    var description: String
+    
+    var duration: DateComponents
+}
+
